613d8dece772d370701a7a3864e5b8dd9b896dd4
himaw <himaw@example.com>
2025-07-28 16:51:13 +0900

    スプレッドシートURLを更新し、ログイン画面のデザインを統一

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 document.getElementById('loginForm').addEventListener('submit', async function(event) {
     event.preventDefault();
 
-    const username = document.getElementById('username').value;
+    const userId = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('errorMessage');
     // ↓↓↓↓ ご自身の新しいウェブアプリのURLに書き換えてください ↓↓↓↓
-    const gasUrl = 'https://script.google.com/macros/s/AKfycbxPLq71hcRs-4l-RD3TVIuacBMQdJWob-91qSQ9EFeK11_Fu6rPIz48CofPbUsXH7vU/exec';
+    const gasUrl = 'https://script.google.com/macros/s/AKfycbwliS2nlVfOs5zx6IdQ8bGyGLovRFUeA8PcrrfbFF1Qkckn9sxxiUSFZHgUQJKb5OXh/exec';
 
     errorMessage.textContent = '';
 
@@ -13,8 +13,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
         const response = await fetch(gasUrl, {
             method: 'POST',
             body: JSON.stringify({ 
-                action: 'login',
-                userId: username,
+                username: userId,
                 password: password
             }),
             headers: { 'Content-Type': 'text/plain' }, // GAS特有のヘッダー
